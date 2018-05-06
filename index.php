52502e4e9c57b1fdc7fdd183483d9057330a0c09
Alek <uitaleks@gmail.com>
2018-05-06 18:13:21 +0400

    Add code to php-file for take out images, descriptions, count of comments and likes, date of publication and tags(if it's exists). Modified html-file: add ajax

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<? include 'add_user.php' ?>
+	<? include 'add_user2.php' ?>
 
 	<header>
 		<div class="container">
@@ -16,142 +16,48 @@
 				<img src="<?=$profile_picture?>" alt="">
 				<div class="users">
 					<div>
-					<h4>Имя пользователя: <?=$full_name?></h4>
-				</div>
+						<h4>Имя пользователя: <?=$full_name?></h4>
+					</div>
 				<div>
 					<p>Количество добавленных фото: <?=$counts?></p>
 				</div>
 				</div>
-				
-
-        		
-        	</div>
-                	
+        	</div>       	
 		</div>
-	
-		
     </header>
 
-    <hr>
-
-    <div class="container">
-
-      <div class="row justify-content-center">
-        <div class="col-md-7 align-self-center">
-          <a href="#">
-            <img class="img-fluid unloaded rounded mb-3 mb-md-0" src="http://placehold.it/700x400" alt="">
-          </a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="row justify-content-center">
-        <div class="col-md-7 align-self-center">
-          <a href="#">
-            <img class="img-fluid unloaded rounded mb-3 mb-md-0" src="http://placehold.it/700x400" alt="">
-          </a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="row justify-content-center">
-        <div class="col-md-7 align-self-center">
-          <a href="#">
-            <img class="img-fluid unloaded rounded mb-3 mb-md-0" src="http://placehold.it/700x400" alt="">
-          </a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="row justify-content-center">
-        <div class="col-md-7 align-self-center">
-          <a href="#">
-            <img class="img-fluid unloaded rounded mb-3 mb-md-0" src="http://placehold.it/700x400" alt="">
-          </a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="row justify-content-center">
-        <div class="col-md-7 align-self-center">
-          <a href="#">
-            <img class="img-fluid unloaded rounded mb-3 mb-md-0" src="http://placehold.it/700x400" alt="">
-          </a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="row justify-content-center">
-        <div class="col-md-7 align-self-center">
-          <a href="#">
-            <img class="img-fluid unloaded rounded mb-3 mb-md-0" src="http://placehold.it/700x400" alt="">
-          </a>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
-        </div>
-      </div>
-
-      <hr>
-
-      
-    </div>
+    <div id="results"></div>
     <div class="text-center">
-    	<button class="btn btn-success">Загрузить ещё</button>
+    	<button id="add_next" class="btn btn-success">Загрузить ещё</button>
     </div>
-</div>
+    <br />
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
 <script type="text/javascript">
-	$.ajax({
-  		url: "add_user.php",
-  		type: 'GET',
-  		success: function(data){
+	
+	function addImages (next, count) {
+		$.ajax({
+  			url: "add_image.php",
+  			type: 'GET',
+  			data: {
+  				next: next,
+  				count: count
+  			},
+  			success: function(data){
+    			$("#results").html(data);
+  			}	
+		});
+	};
 
-    		//$("#results").append(html);
-    		console.log(data)
-    	 // data.data[x].images.low_resolution.url - URL of image, 306х306
-			// data.data[x].images.thumbnail.url - URL of image 150х150
-			// data.data[x].images.standard_resolution.url - URL of image 612х612
-			// data.data[x].link - Instagram post URL 
-			//}
-    		/*for(var i = 0; i < 10; i++) {
-				(function(e) {
-					setTimeout(function() {
-						console.log(e);
-					}, 1000);
-				})(i);
-			}
+	var count = 5;
+	addImages(false, count);
 
-			for( x in data.data ) {
-				(function(x) {
-					$(".img-fluid.unloaded").each(function() {
-						$(this).attr("src", data.data[x].images.standard_resolution.url)
-					})
-				})(x);
-			}
-
-    		/*$(".img-fluid.unloaded").each(function(){
-    			
-    			
-    			for( x in data.data ){
-					//$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
-    				//for (var i = 0; i < html.data.length; i++) {
-    				//console.log(that)
-    				$(this).attr("src", data.data[x].images.standard_resolution.url)
-    			}
-    		})*/
-  		}	
+	$("#add_next").click(function() {
+		count += 5;
+		addImages(true, count);
 	});
+
 </script>	
 </body>
 </html>
